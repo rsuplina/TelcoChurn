0ab0ad64db6ed6b20ece0881bd9429b2ade22000
campossalex <32500181+campossalex@users.noreply.github.com>
2023-12-15 10:33:49 +0100

    Update 1_trainStrategy_job.py

--- 1_trainStrategy_job.py
+++ 1_trainStrategy_job.py
@@ -38,8 +38,6 @@ else:
   #DATABASE = uservariables['username']
   DATABASE = 'acampos'
 
-DATABASE = 'acampos'
-
 runtimes=cml.get_runtimes()
 runtimes=runtimes['runtimes']
 runtimesdf = pd.DataFrame.from_dict(runtimes, orient='columns')
